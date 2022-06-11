2f0bc8199ba2647bf682fa68102796e89f7c3fce
Alicia Sykes <alicia@omg.lol>
2022-06-11 22:36:48 +0100

    🏗 Sets up styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import Styled from 'styled-components';
 import Demo from 'components/Demo';
 import 'App.css';
 
+const Container = Styled.div`
+  background: #bfc;
+  width: fit-content;
+  margin: 1rem auto;
+  padding: 0.5rem 1rem;
+  border-radius: 6px;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
       <h1>Hello 👋</h1>
       <Demo message="React is Dumb," />
-    </div>
+    </Container>
   );
 }
 
