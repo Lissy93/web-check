9952e2d955555ed8587162e60c6bfbdccb86cf76
Alicia Sykes <alicia@omg.lol>
2022-06-25 19:51:06 +0100

    💅 Adds typography styles and assets

---
+++ src/styles/typography.ts
@@ -0,0 +1,12 @@
+export const TextReset = `
+  font-family: PTMono, Helvetica, Arial, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: 0.38px;
+  margin: 0;
+  padding: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`;
