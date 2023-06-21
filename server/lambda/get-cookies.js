2adbd097a1f9f0324b0744a3b5c658c5964c6fd2
Alicia Sykes <alicia@omg.lol>
2023-06-21 14:28:42 +0100

    Writes server functions to get cookied and headers

---
+++ server/lambda/get-cookies.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const { url } = event.queryStringParameters;
+
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'url query string parameter is required' }),
+    };
+  }
+
+  try {
+    const response = await fetch(url);
+    const cookies = response.headers.get('set-cookie');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ cookies }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
+};
---
+++ server/lambda/get-headers.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const { url } = event.queryStringParameters;
+
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'url query string parameter is required' }),
+    };
+  }
+
+  try {
+    const response = await fetch(url);
+    const headers = response.headers.raw();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(headers),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
+};
