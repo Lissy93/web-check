e7dbca42a31ba4bcd33950b642d2339e0e1cdb78
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-07-17 14:57:05 +0530

    fixed some styling bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import Styled from 'styled-components';
 import Home from 'pages/Home';
 import Results from 'pages/Results';
 import About from 'pages/About';
-import colors from 'styles/colors';
-
-const Container = Styled.main`
-  background: ${colors.background};
-  color: ${colors.textColor};
-  width: 100vw;
-  height: 100vh;
-  margin: 0;
-`;
 
 function App() {
   return (
-    <Container>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/results/:address" element={<Results />} />
       </Routes>
-    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import Styled from 'styled-components';
+import colors from 'styles/colors';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+const Container = Styled.main`
+  background: ${colors.background};
+  color: ${colors.textColor};
+  width: 100vw;
+  height: 100%;
+  min-height: 100vh;
+  margin: 0;
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Container>
   </React.StrictMode>
 );
 
