3f1b4bea3aa7da47f11b168b7d58aeed4863af95
Alicia Sykes <alicia@omg.lol>
2023-07-23 23:54:56 +0100

    Integrates error logging

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Styled from 'styled-components';
+import * as Sentry from '@sentry/react';
+
 import Home from 'pages/Home';
 import Results from 'pages/Results';
 import About from 'pages/About';
@@ -13,6 +15,18 @@ const Container = Styled.main`
   height: 100vh;
   margin: 0;
 `;
+
+Sentry.init({
+  dsn: 'https://30eb6135d37643fb95c7da4e77a46142@glitch.as93.net/1',
+  beforeSend(event) { // Check if error logging is disabled
+    const ignoredHosts = ['localhost', '127.0.0.1'];
+    const disableErrors = process.env.REACT_APP_DISABLE_ERROR_LOGGING;
+    if (disableErrors || ignoredHosts.includes(window.location.hostname)) {
+        return null;
+    }
+    return event;
+  }
+});
 
 function App() {
   return (
