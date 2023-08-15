e24934b7ddeb32f5209df09d92515cc47f4d8776
Alicia Sykes <alicia@omg.lol>
2023-08-15 22:25:27 +0100

    Button layout for mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -69,6 +69,9 @@ const SiteFeaturesWrapper = styled(StyledCard)`
         width: calc(100% - 2rem);
       }
     }
+    @media(max-width: 600px) {
+      flex-wrap: wrap;
+    }
   }
   ul {
     -webkit-column-width: 150px;
