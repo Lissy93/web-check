f4dd5d7a315e4f1a924855b737e7f64c00893ce2
Alicia Sykes <alicia@omg.lol>
2024-02-28 20:06:07 +0000

    Remove commented out duplicate line

--- src/components/Results/SslCert.tsx
+++ src/components/Results/SslCert.tsx
@@ -90,8 +90,6 @@ const SslCertCard = (props: { data: any, title: string, actionButtons: any }): J
       { valid_from && <DataRow lbl="Renewed" val={formatDate(valid_from)} /> }
       { serialNumber && <DataRow lbl="Serial Num" val={serialNumber} /> }
       { fingerprint && <DataRow lbl="Fingerprint" val={fingerprint} /> }
-      {/* // this line is repeated 
-      { fingerprint && <DataRow lbl="Fingerprint" val={fingerprint} /> } */}
       { ext_key_usage && <ListRow title="Extended Key Usage" list={getExtendedKeyUsage(ext_key_usage)} /> }
       
     </Card>
