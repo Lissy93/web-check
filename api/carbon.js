5ec6f4d22e82ed44cea679371f08e1d7c46ee977
Mouad Kimdil <mkimdil@e3r1p1.1337.ma>
2024-02-29 10:23:49 +0100

    Refactored code to use axios instead of https

--- api/carbon.js
+++ api/carbon.js
@@ -1,38 +1,15 @@
-const https = require('https');
+const axios = require('axios');
 const middleware = require('./_common/middleware');
 
 const handler = async (url) => {
-
-  // First, get the size of the website's HTML
-  const getHtmlSize = (url) => new Promise((resolve, reject) => {
-    https.get(url, res => {
-      let data = '';
-      res.on('data', chunk => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        const sizeInBytes = Buffer.byteLength(data, 'utf8');
-        resolve(sizeInBytes);
-      });
-    }).on('error', reject);
-  });
-
   try {
-    const sizeInBytes = await getHtmlSize(url);
+    // First, get the size of the website's HTML
+    const { data: html } = await axios.get(url);
+    const sizeInBytes = Buffer.byteLength(html, 'utf8');
     const apiUrl = `https://api.websitecarbon.com/data?bytes=${sizeInBytes}&green=0`;
 
     // Then use that size to get the carbon data
-    const carbonData = await new Promise((resolve, reject) => {
-      https.get(apiUrl, res => {
-        let data = '';
-        res.on('data', chunk => {
-          data += chunk;
-        });
-        res.on('end', () => {
-          resolve(JSON.parse(data));
-        });
-      }).on('error', reject);
-    });
+    const { data: carbonData } = await axios.get(apiUrl);
 
     if (!carbonData.statistics || (carbonData.statistics.adjustedBytes === 0 && carbonData.statistics.energy === 0)) {
       return {
