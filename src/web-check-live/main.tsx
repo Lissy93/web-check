bb082aa9db9c6968dba6c9ddf967a58acbc83269
Alicia Sykes <alicia@omg.lol>
2026-05-05 12:26:55 +0100

    fix: Cloud deployment astro hydration

--- src/web-check-live/main.tsx
+++ src/web-check-live/main.tsx
@@ -1,8 +1,6 @@
-import { BrowserRouter, StaticRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 
-export default ({ pathname }: { pathname: string }) => (
-    import.meta.env.SSR
-        ? <StaticRouter location={pathname}><App/></StaticRouter>
-        : <BrowserRouter><App/></BrowserRouter>
+export default () => (
+  <BrowserRouter><App/></BrowserRouter>
 )
